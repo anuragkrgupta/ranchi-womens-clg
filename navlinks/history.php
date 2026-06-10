286cb33a186ea83c1cd6bf86f56582dc603731c1
Anurag kumar <anuragkumar1252003@gmail.com>
2026-06-10 12:50:13 +0530

    history page updated

---
+++ navlinks/history.php
@@ -0,0 +1,119 @@
+<?php
+include('../include/header.php');
+include('../include/navbar.php');
+?>
+<body>
+    
+<!-- Image Banner Section -->
+<section class="inner-banner">
+    <img src="../assets/history/imgg.png" alt="Banner Image">
+
+    <div class="inner-banner-overlay">
+        <h1>History of College</h1>
+    </div>
+</section>
+
+
+<!-- Text Content Section -->
+<section class="content-section">
+    <div class="content-container">
+
+        <p>
+            With <strong>11 students and 03 Teachers</strong> the Visionary Founder Principal, 
+            <strong>Mrs. Bhanumati Prasad</strong> started nurturing a seedling in the-then 
+            <strong>Mohsin Manjilat Circular Road, Ranchi</strong> in the propitious year 
+            <strong>1949 (July)</strong> that has grown as multi-faculty Premier College 
+            <strong>'Ranchi Women's College'</strong> enlightening the whole State as 
+            <strong>'Capacity Builders for Women'</strong>.
+        </p>
+
+        <p>
+            Initially, <strong>Ranchi Women's College</strong> was affiliated to the 
+            <strong>Patna University in 1951</strong>. In <strong>1957</strong> the affiliation was shifted to the 
+            <strong>Bihar University</strong> which was carved out of Patna University as an independent unit. 
+            Finally, when <strong>Ranchi University</strong> emerged as an independent University in 
+            <strong>1960</strong>, Ranchi Women's College was affiliated to it.
+        </p>
+
+        <p>
+            Ranchi Women's College is a <strong>Government aided College</strong> which got recognition under 
+            <strong>U.G.C Act 1956 as 2(f) & 12 B</strong>. In <strong>1987</strong> it was transferred to the category of 
+            <strong>“College Teaching up to Post Graduate Degree”</strong> by UGC through letter no. 
+            <strong>F 8-105/86 (CPP-1) dated 3rd Nov. 1987</strong> under section 2(f) UGC Act 1956.
+        </p>
+
+        <p>
+            Ranchi Women's College, Ranchi was started in <strong>1949</strong> with a vision to lead from 
+            <strong>“darkness to light”</strong> which was reflected in its logo also. At that time 
+            <strong>Jharkhand was combined with Bihar</strong> known as 
+            <strong>Chhotanagpur or South Bihar</strong>. Ranchi, a part of South Chhotanagpur was a hilly area 
+            and was famous as a <strong>hill station</strong>. It was known as 
+            <strong>summer capital of Bihar</strong> and the Governor and other headquarters used to shift to Ranchi 
+            every year during the summer season. For this a Governor House and a number of bungalows were 
+            constructed here during the British rule. The climate of this place attracted a number of tourists here. 
+            Most of Bengali and South Indian tourists settled down here due to its climate. During British rule two 
+            more important institutions were established here due to its climate – 
+            <strong>T.B. Sanatorium and Mental Hospital</strong>. People from all over India were sent to these 
+            institutions for healing body and mind.
+        </p>
+
+        <p>
+            During the period of the <strong>British Raj</strong>, adivasis and the local population of Ranchi and 
+            other regions continued to oppose the subjugation by the British, and the region witnessed a number of 
+            uprisings and revolts.
+        </p>
+
+        <p>
+            Struggle for freedom brought many visionary leaders in frontline by that time in South Chhotanagpur who 
+            fought not only for freedom in terms of sovereignty but also for uplift of society. By 
+            <strong>1944 St. Xavier’s College</strong> was established in Ranchi. But there was no scope for 
+            <strong>women’s education</strong>. It was during that period that a visionary group of philanthropists, 
+            <strong>Mrs. Bhanumati Prasad</strong>, <strong>Mrs. A. V. Davis</strong>, 
+            <strong>Mr. K. B. Sahay</strong>, <strong>Mr. M. S. Rao (ICS)</strong>, 
+            <strong>Mr. S. N. Ganguly</strong>, <strong>Mr. Hemen Ganguly</strong>, 
+            <strong>Mr. Suresh Prasad</strong> and <strong>Mr. S. K. Bage</strong> made up their mind to provide the 
+            opportunity of <strong>higher education to the unprivileged women of Chhotanagpur</strong> with a firm 
+            vision for <strong>“Enlightenment for Women”</strong>.
+        </p>
+
+        <p>
+            They all aimed at bringing forth the women of <strong>tribal, SC and O.B.C. categories</strong> of this 
+            neglected part into the mainstream of the society from their marginalized status through 
+            <strong>education</strong>. Their combined and continued efforts took the shape of 
+            <strong>“Ranchi Women's College” in July 1949</strong> when 
+            <strong>Mrs. Bhanumati Prasad as Principal</strong> started this College with 
+            <strong>three Teachers</strong> in Mohsin Manjil at Circular Road, Ranchi with 
+            <strong>eleven students of Pre University Classes</strong>.
+        </p>
+
+        <p>
+            In a short span this College became a <strong>premier institute of combined Bihar</strong> and attracted 
+            students from neighboring states like <strong>West Bengal, Odisha and Uttar Pradesh</strong>. It achieved 
+            the reputation as a torch bearer to attain freedom from the darkness of ignorance and misunderstanding 
+            leading the girls to a luminous world of <strong>wisdom, knowledge and independence</strong>. In due 
+            course the students achieved the ranks of <strong>Justice, Vice Chancellor, Pro Vice Chancellor, 
+            Scientists, I.A.S., I.P.S., Bankers, Professors, Administrative Officers, Sportspersons and Leaders</strong>.
+        </p>
+
+        <p>
+            <strong>Jharkhand got the status of an independent state in the year 2000</strong>. Ranchi, the summer 
+            capital of combined Bihar, was announced as the <strong>Capital of Jharkhand</strong>. According to the 
+            <strong>2011 Census report</strong>, Ranchi district had a population of 
+            <strong>29,12,022</strong>, a sex ratio of <strong>950 females to 1000 males</strong> and a literacy 
+            rate of <strong>77.13%</strong> which was higher than the national average of 74.04%.
+        </p>
+
+        <p>
+            Ranchi Women's College, Ranchi has contributed significantly to this progress. The college that was 
+            started with <strong>eleven students in 1949</strong> is offering 
+            <strong>forty two programme options at UG & PG level</strong> today to approximately 
+            <strong>10,000 students</strong>, continuing its remarkable legacy of 
+            <strong>women empowerment through higher education</strong>.
+        </p>
+
+    </div>
+</section>
+</body>
+
+
+<?php include('../include/footer.php'); ?>
